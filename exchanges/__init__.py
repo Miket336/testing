ee67e04979274e90f85c2707e48bffc9c6dc6e79
Miket336 <135051891+Miket336@users.noreply.github.com>
2026-06-14 02:00:49 +0100

    Delete exchanges directory

--- exchanges/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Exchange API modules for accessing betting exchange data.
-"""
-
-from .smarkets_api import SmarketsAPI
-from .matchbook_api import MatchbookAPI
-
-__all__ = ['SmarketsAPI', 'MatchbookAPI']
